b5366175c9ae6fcfc9e581dfa818bcbe8d11bbb5
rory <rorymalone@live.com>
2023-05-17 01:42:48 +0100

    Table component created and implemented on the products index

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -9,50 +9,25 @@
         <a href="{{route('products.create')}}">Add Product</a>
     </div>
 
-    <div class="py-12">        
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" class="px-6 py-3">
-                            Product name
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Description
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Category
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Price
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Action
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($products as $product)
-                    <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">                                
-                            <td class="px-6 py-4">
-                                {{ $product->title }}
-                            </td>
-                            <td class="px-6 py-4">
-                                {{ $product->description }}
-                            </td>
-                            <td class="px-6 py-4">
-                                {{ $product->category->title }}
-                            </td>
-                            <td class="px-6 py-4">
-                                {{ $product->cost }}
-                            </td>
-                            <td class="px-6 py-4">
-                                <a href="{{ route('products.edit',  $product->id)}}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                            </td>                                           
-                    </tr>
-                    @endforeach                  
-                </tbody>
-            </table>
-        </div>
-    </div>    
+    <x-table.table :headers="['title','description','category','cost','action']">
+        @foreach ($products as $product)
+            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">                                
+                <x-table.table-data>
+                    {{ $product->title }}
+                </x-table.table-data>
+                <x-table.table-data>
+                    {{ $product->description }}
+                </x-table.table-data>
+                <x-table.table-data>
+                    {{ $product->category->title }}
+                </x-table.table-data>
+                <x-table.table-data>
+                    {{ $product->cost }}
+                </x-table.table-data>
+                <x-table.table-data>
+                    <a href="{{ route('products.edit',  $product->id)}}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </x-table.table-data>                                           
+            </tr>
+        @endforeach 
+    </x-table.table>
 </x-app-layout>
