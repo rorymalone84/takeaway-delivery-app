2adcc4b2ddc8e2b949b182195a474caca6ea3169
rory <rorymalone@live.com>
2023-07-07 02:09:57 +0100

    Tightened up Order validations, and refactored controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\OrderRequest;
 use Session;
 use App\Models\Order;
 use App\Models\OrderProduct;
+use App\Services\OrderService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Auth;
@@ -14,7 +16,8 @@ class OrderController extends Controller
 {
 
     // order index for admin area
-    public function index(){
+    public function index()
+    {
 
         return view('orders.index', [
             'orders' => Order::all()
@@ -22,7 +25,8 @@ class OrderController extends Controller
     }
 
     //show/edit an individual order from admin area, with products
-    public function show($id){
+    public function show($id)
+    {
 
         $order_products = Order::find($id)->products;
 
@@ -32,38 +36,21 @@ class OrderController extends Controller
         ]);
     }
 
-    public function checkout(){
+    public function checkout()
+    {
 
         return view('orders.checkout', [
             'user' => Auth::user(),
         ]);
     }
 
-    public function store(Request $request){
+    public function store(OrderRequest $request, OrderService $orderService)
+    {
+        $total_price = $orderService->getTotal($request);
 
-        $total_price = 0;
+        $order = Order::create($request->all() + ['total_price' => $total_price, 'status' => 'pending']);
 
-        foreach (session('cartProducts') as $product){
-            $total_price += $product['price'] * $product['quantity'];
-        }
-
-        $total_price += $request->input('delivery_price');
-
-        // Insert into orders table
-        $order = Order::create([
-            'user_id' => Auth::user()->id ?? null,
-            'customer_name' => $request->input('customer_name'),
-            'customer_address' => $request->input('customer_address'),
-            'customer_email' => $request->input('customer_email'),
-            'customer_city' => $request->input('customer_city'),
-            'customer_phone' => $request->input('customer_phone'),
-            'delivery_price' => $request->input('delivery_price'),
-            'total_price' => $total_price,
-            'store_id' => $request->input('store_id'),
-            'status' => 'pending'
-        ]);
-
-        foreach (session('cartProducts') as $product){
+        foreach (session('cartProducts') as $product) {
             OrderProduct::create([
                 'order_id' => $order->id,
                 'product_id' => $product['id'],
@@ -76,12 +63,8 @@ class OrderController extends Controller
         Session::forget('order');
         Session::save();
 
-        $url = URL::temporarySignedRoute('storefront.confirmation', now()->addMinutes(5),[
-            'id'=>$order
-        ]);
-
-        return Redirect::to($url);
-
+        return Redirect::to(URL::temporarySignedRoute('storefront.confirmation', now()->addMinutes(5), [
+            'id' => $order
+        ]));
     }
-
 }
